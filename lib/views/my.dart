248e29ec8ef714a3b4ccb0af61b7d071c4186f3c
fei <1109614355@qq.com>
2019-05-16 23:32:44 +0800

    开发我的页面

--- lib/views/my.dart
+++ lib/views/my.dart
@@ -21,9 +21,56 @@ class MyState extends State<My> {
 
   @override
   Widget build(BuildContext context) {
-    print(state.params.page.toString() +
-        'My>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-    return Text(state.params.page.toString());
+    return Container(
+      padding: const EdgeInsets.only(top: 40.0),
+      child: Column(
+        children: <Widget>[
+          Center(
+              // child: DecoratedBox(
+              //   child:
+              //     Image.network(
+              //       'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1638079650,2146947483&fm=27&gp=0.jpg',
+              //       height: 50,
+              //       width: 50,
+              //       fit: BoxFit.cover
+              //     ),
+              //     decoration: BoxDecoration(
+              //       borderRadius: BorderRadius.all(
+              //       const Radius.circular(50.0),
+              //     ),),
+              // ),
+              child: Container(
+            width: 50.0,
+            height: 50.0,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage(
+                  'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1638079650,2146947483&fm=27&gp=0.jpg',
+                ),
+              ),
+            ),
+          )
+              //   child: Container(
+              //     height: 50,
+              //     width: 50,
+              //     child:
+              //     Image.network(
+              //       'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1638079650,2146947483&fm=27&gp=0.jpg',
+              //       height: 50,
+              //       width: 50,
+              //       fit: BoxFit.cover),
+              //     decoration: new BoxDecoration(
+              //       color: Colors.red[400],
+              //       borderRadius: new BorderRadius.all(
+              //         const Radius.circular(50.0),
+              //       ),
+              //     ),
+              // )
+              )
+        ],
+      ),
+    );
   }
 }
 
