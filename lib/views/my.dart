92a01b2cc32e97b30856603956fa9011fef1c5e5
fei <1109614355@qq.com>
2019-05-16 23:32:56 +0800

    开发我的页面

--- lib/views/my.dart
+++ lib/views/my.dart
@@ -26,48 +26,26 @@ class MyState extends State<My> {
       child: Column(
         children: <Widget>[
           Center(
-              // child: DecoratedBox(
-              //   child:
-              //     Image.network(
-              //       'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1638079650,2146947483&fm=27&gp=0.jpg',
-              //       height: 50,
-              //       width: 50,
-              //       fit: BoxFit.cover
-              //     ),
-              //     decoration: BoxDecoration(
-              //       borderRadius: BorderRadius.all(
-              //       const Radius.circular(50.0),
-              //     ),),
-              // ),
               child: Container(
             width: 50.0,
             height: 50.0,
             decoration: BoxDecoration(
               shape: BoxShape.circle,
               image: DecorationImage(
-                image: AssetImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(
                   'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1638079650,2146947483&fm=27&gp=0.jpg',
                 ),
               ),
             ),
-          )
-              //   child: Container(
-              //     height: 50,
-              //     width: 50,
-              //     child:
-              //     Image.network(
-              //       'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1638079650,2146947483&fm=27&gp=0.jpg',
-              //       height: 50,
-              //       width: 50,
-              //       fit: BoxFit.cover),
-              //     decoration: new BoxDecoration(
-              //       color: Colors.red[400],
-              //       borderRadius: new BorderRadius.all(
-              //         const Radius.circular(50.0),
-              //       ),
-              //     ),
-              // )
-              )
+          ),
+              ),
+            Container(
+              padding: EdgeInsets.all(20),
+              width: 375,
+              child: Text('jkfdshfkdshkfds'),
+              
+            )
         ],
       ),
     );
